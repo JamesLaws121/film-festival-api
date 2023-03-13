2afba295cd0d50483747b53869decc706217afff
James Laws <jla201@uclive.ac.nz>
2023-03-13 13:01:15 +1300

    Added user type
    created user_types file to handle user type

---
+++ src/app/user_types.d.ts
@@ -0,0 +1,11 @@
+type User = {
+    /**
+     * User id as defined by the database
+     */
+    id: number,
+    first_name : string,
+    last_name : string,
+    email : string,
+    password : string,
+    image_filename : string,
+}
